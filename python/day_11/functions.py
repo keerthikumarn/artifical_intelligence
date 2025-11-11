2304972946fc70d1195506e49893226ae883ddf7
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-11 21:09:49 +0530

    started with functions

---
+++ python/day_11/functions.py
@@ -0,0 +1,17 @@
+def generate_full_name():
+   first_name = "Keerthi"
+   last_name = "Kumar"
+   space = " "
+   full_name = first_name + space + last_name
+   print(full_name)
+   
+def add_two_numbers ():
+    num_one = 21
+    num_two = 76
+    total = num_one + num_two
+    print(total)
+    
+
+add_two_numbers()
+   
+generate_full_name()
