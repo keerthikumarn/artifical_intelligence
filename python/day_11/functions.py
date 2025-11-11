8d4b3b2580328bb764adace5443cbcd32bb10019
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-11 21:46:13 +0530

    started with functions with return

--- python/day_11/functions.py
+++ python/day_11/functions.py
@@ -3,15 +3,17 @@ def generate_full_name():
    last_name = "Kumar"
    space = " "
    full_name = first_name + space + last_name
-   print(full_name)
+   return full_name
+   #print(full_name)
    
 def add_two_numbers ():
     num_one = 21
     num_two = 76
     total = num_one + num_two
-    print(total)
+    #print(total)
+    return total
     
 
-add_two_numbers()
+print(add_two_numbers())
    
-generate_full_name()
+print(generate_full_name())
