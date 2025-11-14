1ee9f0147c756bb975296fe49d672d24a59e4443
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-14 09:48:30 +0530

    included math functions

--- python/day_12/main.py
+++ python/day_12/main.py
@@ -1,5 +1,6 @@
 import modules
 from statistics import *
+import math
 
 print(modules.generate_full_name('Keerthi', 'Kumar')) 
 
@@ -9,3 +10,9 @@ print("Mean value of ages :", mean(ages))
 print("Median value of ages :", median(ages))
 print("stdev value of ages :", stdev(ages))
 print("Mode value of ages :", mode(ages))
+
+print("PI value: ", math.pi)
+print("Power of: ", math.pow(2, 3))
+print("Floor value: ", math.floor(9.81)) 
+print("Rounding of the value: ", math.ceil(9.81))
+print("Logarithm value: ", math.log10(100))
