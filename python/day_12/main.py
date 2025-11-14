f54307b46c8aa2bdd966bf6eb2dfabb09e2dd69f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-14 09:45:14 +0530

    included built-in function statistics

--- python/day_12/main.py
+++ python/day_12/main.py
@@ -1,3 +1,11 @@
 import modules
+from statistics import *
 
 print(modules.generate_full_name('Keerthi', 'Kumar')) 
+
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+
+print("Mean value of ages :", mean(ages))
+print("Median value of ages :", median(ages))
+print("stdev value of ages :", stdev(ages))
+print("Mode value of ages :", mode(ages))
