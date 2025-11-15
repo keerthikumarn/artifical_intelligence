940ccb9e878a3a70cde7e30dcd8fc16b9a9a988f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-15 21:30:56 +0530

    started with list with numbers

--- python/day_13/list_comprehension.py
+++ python/day_13/list_comprehension.py
@@ -8,3 +8,12 @@ print(my_list)
 new_list = [lang for lang in language]
 print(type(new_list))
 print(new_list)
+
+# Generating numbers
+numbers = [number for number in range(15)]
+print(type(numbers))
+print(numbers)
+
+# printing square of numbers
+squares = [number * number for number in range(10)]
+print(squares)
