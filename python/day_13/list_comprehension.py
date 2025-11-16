88df9b2b0bfd95917ac3bfdef19f0d754685d88a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-16 17:09:54 +0530

    flattened latest

--- python/day_13/list_comprehension.py
+++ python/day_13/list_comprehension.py
@@ -21,3 +21,8 @@ print(squares)
 # List of tuples
 tuples = [(number, number * number) for number in range (11)]
 print(tuples)
+
+# Flattening a three dimensional array
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [ number for row in list_of_lists for number in row]
+print(flattened_list)
