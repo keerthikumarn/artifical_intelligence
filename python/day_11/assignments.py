1cccf06e0362e04eacec8f57ee7e82c0fe92c91c
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-21 22:45:12 +0530

    assignments-function args

--- python/day_11/assignments.py
+++ python/day_11/assignments.py
@@ -4,3 +4,15 @@ def area_of_circle(r):
   return math.pi * r * r
   
 print(area_of_circle(5)) 
+
+
+def add_all_nums(* args):
+  total_sum = 0
+  for value in args:
+     if not isinstance(value, (int, float)):
+        return f"Invalid input: '{value}' is not a number."
+     total_sum+=value
+  return total_sum
+  
+print(add_all_nums(11, 22, 33, 44.5))
+print(add_all_nums(101, "k", 76)) 
