37b874300d62526261b0fec515b98cb26bc64432
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-22 19:11:01 +0530

    higher order functions

---
+++ python/day_14/higher_order_functions.py
@@ -0,0 +1,26 @@
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def absolute(x):
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type):
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+
+result = higher_order_function('square')
+print(result(5))
+result = higher_order_function('cube')
+print(result(9))
+result = higher_order_function('absolute')
+print(result(-4))
