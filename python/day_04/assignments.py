2f327ed4a3588c216f1de34d4f912483c4f07b3e
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-23 21:33:21 +0530

    day_4: assignments

--- python/day_04/assignments.py
+++ python/day_04/assignments.py
@@ -37,7 +37,14 @@ Keerthi   40      India     Bangalore
 print("Name\tAge\tCountry\tCity")
 print("Keerthi\t40\tIndia\tBangalore")
 
-
+'''
+radius = 10
+area = 3.14 * radius ** 2
+The area of a circle with radius 10 is 314 meters square.
+'''
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area} meters square.")
 
 
 
