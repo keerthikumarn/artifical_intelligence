d65a6ad2e884c978c4419f7258dc60edf2d1558e
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-24 11:31:12 +0530

    day_05: assigments

--- python/day_05/assignments.py
+++ python/day_05/assignments.py
@@ -3,3 +3,12 @@ Declare a list called mixed_data_types, put your(name, age, height, marital stat
 '''
 mixed_data_types = ["Keerthi Kumar N", 40, 5.4, "Married", "Bangalore, KA, INDIA"]
 print(mixed_data_types)
+
+
+first_item = mixed_data_types[0]
+middle_item = mixed_data_types[len(mixed_data_types) // 2]
+last_item = mixed_data_types[-1]
+
+print("First Element:", first_item)
+print("Middle Element:", middle_item)
+print("Last Element:", last_item)
