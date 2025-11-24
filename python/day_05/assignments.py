dfb55106c442dde392b1c3b0550a87ebf33eb939
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-24 11:46:42 +0530

    day_05: assigments

--- python/day_05/assignments.py
+++ python/day_05/assignments.py
@@ -12,3 +12,9 @@ last_item = mixed_data_types[-1]
 print("First Element:", first_item)
 print("Middle Element:", middle_item)
 print("Last Element:", last_item)
+
+'''
+Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+'''
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+print("List of IT companies: ", it_companies)
