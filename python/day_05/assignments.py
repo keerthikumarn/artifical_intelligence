17cc73e718bcd55756d7bfb57f06709b8ce734d6
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-25 11:46:42 +0530

    day_05: assigments

--- python/day_05/assignments.py
+++ python/day_05/assignments.py
@@ -18,3 +18,14 @@ Declare a list variable named it_companies and assign initial values Facebook, G
 '''
 it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 print("List of IT companies: ", it_companies)
+print("No. of companies in the list: ",len(it_companies))
+
+# Print the first, middle and last company
+first_company = it_companies[0]
+middle_company = it_companies[len(it_companies) // 2]
+last_company= it_companies[-1]
+
+print("First Company:", first_company)
+print("Middle Company:", middle_company)
+print("Last Company:", last_company)
+
