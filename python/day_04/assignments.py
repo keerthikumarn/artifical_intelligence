97522dd4af33d40411a7a3087ddcc93f663075f8
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-11-29 21:09:59 +0530

    day4 - assignments

---
+++ python/day_04/assignments.py
@@ -0,0 +1,4 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+words = ["Thirty", "Days", "Of", "Python"]
+result = " ".join(words)
+print(result)
