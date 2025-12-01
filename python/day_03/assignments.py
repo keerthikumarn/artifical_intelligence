be8fcea182c18838e83afc1fd3c83fb5d2946fb6
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-01 21:39:45 +0530

    day3 - assignments

--- python/day_03/assignments.py
+++ python/day_03/assignments.py
@@ -8,8 +8,19 @@ height = 5.8
 complex_number = 3 + 4j
 
 # Prompt the user to enter base and height of a triangle and calculate its area
-base = float(input("Enter the base of the triangle: "))
-tri_height = float(input("Enter the height of the triangle: "))
+#base = float(input("Enter the base of the triangle: "))
+#tri_height = float(input("Enter the height of the triangle: "))
+base = 30
+tri_height=15
 area = 0.5 * base * tri_height
 print("The area of the triangle is:", area)
 
+'''
+Compute the perimeter of the triangle
+'''
+side_a = float(input("Enter the side A of the triangle: "))
+side_b = float(input("Enter the side B of the triangle: "))
+side_c = float(input("Enter the side C of the triangle: "))
+
+perimeter = side_a + side_b + side_c
+print("Perimeter of the triangle is: ", perimeter)
