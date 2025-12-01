339db8b70f9e36ccab2ff55d0f580b93043d0d4c
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-01 21:33:19 +0530

    day3 - assignments

---
+++ python/day_03/assignments.py
@@ -0,0 +1,15 @@
+# Declare your age as an integer variable
+age = 40
+
+# Declare your height as a float variable
+height = 5.8
+
+# Declare a variable that stores a complex number
+complex_number = 3 + 4j
+
+# Prompt the user to enter base and height of a triangle and calculate its area
+base = float(input("Enter the base of the triangle: "))
+tri_height = float(input("Enter the height of the triangle: "))
+area = 0.5 * base * tri_height
+print("The area of the triangle is:", area)
+
