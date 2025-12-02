f6ebba3dbcc2ca4105ca9d50906c91b498da2dff
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-02 11:59:08 +0530

    functions with params

--- python/day_14/higher_order_functions.py
+++ python/day_14/higher_order_functions.py
@@ -83,3 +83,18 @@ def split_string_decorator(function):
 def greeting():
     return 'Welcome to the world of python programming!'
 print(greeting())
+
+
+# Accepting Parameters in Decorator Functions
+def decorator_with_parameters(function):
+    def wrapper_accepting_parameters(para1, para2, para3):
+        function(para1, para2, para3)
+        print("I live in {}".format(para3))
+    return wrapper_accepting_parameters
+
+@decorator_with_parameters
+def print_full_name(first_name, last_name, country):
+    print("I am {} {}. I love to teach.".format(
+        first_name, last_name, country))
+
+print_full_name("Keerthi Kumar", "",'Bangalore')
