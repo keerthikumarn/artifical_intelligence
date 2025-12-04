376c079ec6937ef05c58af3cc693c6e9645be66d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-04 15:26:59 +0530

    day_05: assigments

---
+++ python/day_05/assignments.py
@@ -0,0 +1,5 @@
+'''
+Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+'''
+mixed_data_types = ["John Doe", 32, 5.9, "Married", "123 Main Street, New York"]
+print(mixed_data_types)
