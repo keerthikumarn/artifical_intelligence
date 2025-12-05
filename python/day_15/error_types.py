98549e8b1747c9bd0cd11236b2e4e02937db862b
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-05 21:54:22 +0530

    day 15: types of errors

---
+++ python/day_15/error_types.py
@@ -0,0 +1,20 @@
+# NameError
+myage=40
+print(myage)
+
+
+#IndexError
+ages = [40, 12, 78, 9, 65]
+# print(ages[5])
+print(ages[3])
+
+# ModuleNotFoundError
+# import maths
+
+# AttributeError
+import math
+# math.PI
+
+# KeyError
+user = {'name':'Keerthi Kumar N', 'age':40, 'country':'India'}
+user['email']
