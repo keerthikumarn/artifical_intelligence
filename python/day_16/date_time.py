49dd0c630e9c6fc902f76ca053f705f1bc135471
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-06 21:59:08 +0530

    day 16: date and time

---
+++ python/day_16/date_time.py
@@ -0,0 +1,18 @@
+import datetime
+from datetime import datetime
+
+print(dir(datetime))
+
+now = datetime.now()
+print(now)
+
+day = now.day
+month = now.month
+year = now.year
+hour = now.hour
+minute = now.minute
+second = now.second
+timestamp = now.timestamp()
+print(day, month, year, hour, minute)
+print('timestamp', timestamp)
+print(f'{day}/{month}/{year}, {hour}:{minute}')
