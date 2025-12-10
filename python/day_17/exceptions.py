b7a358dfbd9b2d8e83d854ae89e2138391d2ff3e
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-10 10:43:45 +0530

    day 17: exceptions

---
+++ python/day_17/exceptions.py
@@ -0,0 +1,24 @@
+try:
+    print(10 + '5')
+except:
+    print('Something went wrong')
+    
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except:
+    print('Something went wrong')
+    
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
