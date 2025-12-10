14bb7ae72493d41a87830eb817e5319662763f36
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-10 10:45:08 +0530

    day 17: exceptions

--- python/day_17/exceptions.py
+++ python/day_17/exceptions.py
@@ -22,3 +22,19 @@ except ValueError:
     print('Value error occured')
 except ZeroDivisionError:
     print('zero division error occured')
+    
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
