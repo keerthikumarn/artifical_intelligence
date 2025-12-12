beda39532cad5e21ac86e07ae7051b23df889f34
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-12 10:27:32 +0530

    day 17: spreading in python

--- python/day_17/exceptions.py
+++ python/day_17/exceptions.py
@@ -54,3 +54,18 @@ print(ind, aus, fra, rest)
 numbers = [1, 2, 3, 4, 5, 6, 7]
 one, *middle, last = numbers
 print(one, middle, last)   
+
+
+# Spreading in python
+lst_one = [1, 2, 3]
+lst_two = [4, 5, 6, 7]
+lst = [0, *lst_one, *lst_two]
+print(lst)          # [0, 1, 2, 3, 4, 5, 6, 7]
+country_lst_one = ['Finland', 'Sweden', 'Norway']
+country_lst_two = ['Denmark', 'Iceland']
+nordic_countries = [*country_lst_one, *country_lst_two]
+print(nordic_countries) 
+
+
+
+
