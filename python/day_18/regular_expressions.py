b84ade492e3ad723b38bbdfd9852aae6c5362c3f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-14 11:32:12 +0530

    day 18: regular expr

--- python/day_18/regular_expressions.py
+++ python/day_18/regular_expressions.py
@@ -63,6 +63,12 @@ print(matches)  # ['Apple', 'apple']
 regex_pattern = r'[Aa]pple'  # this mean the first letter could be Apple or apple
 matches = re.findall(regex_pattern, txt)
 print(matches)  # ['Apple', 'apple']
+
+
+regex_pattern = r'[Aa]pple' # this square bracket mean either A or a
+txt = 'Apple and banana are fruits. An old cliche says an apple a day a doctor way has been replaced by a banana a day keeps the doctor far far away.'
+matches = re.findall(regex_pattern, txt)
+print(matches)
 txt = '''I am teacher and  I love teaching.
 There is nothing as rewarding as educating and empowering people.
 I found teaching more interesting than any other jobs.
