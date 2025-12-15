cfbfc63d5684a1aa0831d0fbabeba7dfe93e9507
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-15 09:26:10 +0530

    day 18: regular expr

--- python/day_18/regular_expressions.py
+++ python/day_18/regular_expressions.py
@@ -101,3 +101,7 @@ txt = '''Apple and banana are fruits'''
 matches = re.findall(regex_pattern, txt)
 print(matches)
 
+regex_pattern = r'[a].+'  # . any character, + any character one or more times 
+matches = re.findall(regex_pattern, txt)
+print(matches) 
+
