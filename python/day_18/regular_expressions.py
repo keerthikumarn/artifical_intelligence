39781ebe7152936156f09cb1259b77e8459779ce
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-15 09:25:23 +0530

    day 18: regular expr

--- python/day_18/regular_expressions.py
+++ python/day_18/regular_expressions.py
@@ -93,4 +93,11 @@ print(matches)
 regex_pattern = r'\d+'  # d is a special character which means digits, + mean one or more times
 txt = 'This regular expression example was made on December 6,  2019 and revised on July 8, 2021'
 matches = re.findall(regex_pattern, txt)
-print(matches) 
+print(matches)
+
+# Period(.)
+regex_pattern = r'[a].'
+txt = '''Apple and banana are fruits'''
+matches = re.findall(regex_pattern, txt)
+print(matches)
+
