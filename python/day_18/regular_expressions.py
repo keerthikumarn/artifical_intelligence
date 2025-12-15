73ce5c71fcb9973d52d238f347235cc3051e86ff
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-15 09:24:04 +0530

    day 18: regular expr

--- python/day_18/regular_expressions.py
+++ python/day_18/regular_expressions.py
@@ -88,3 +88,9 @@ regex_pattern = r'\d'
 txt = 'This regular expression example was made on December 6,  2019 and revised on July 8, 2021'
 matches = re.findall(regex_pattern, txt)
 print(matches)
+
+# Better output ones
+regex_pattern = r'\d+'  # d is a special character which means digits, + mean one or more times
+txt = 'This regular expression example was made on December 6,  2019 and revised on July 8, 2021'
+matches = re.findall(regex_pattern, txt)
+print(matches) 
