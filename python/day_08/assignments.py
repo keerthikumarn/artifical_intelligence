01594c724acf9f2733cf9d3d252f944063c14693
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-26 10:33:32 +0530

    day_08: assignments

---
+++ python/day_08/assignments.py
@@ -0,0 +1,12 @@
+'''
+Create an empty dictionary called dog Add name, color, breed, legs, age to the dog dictionary
+'''
+dog = {}
+
+dog['name'] = 'Buddy'
+dog['color'] = 'Brown'
+dog['breed'] = 'Labrador'
+dog['legs'] = 4
+dog['age'] = 3
+
+print(dog)
