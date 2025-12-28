7f82f0c418e825afbe4b69e36124483665b03be3
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-12-28 11:21:09 +0530

    day_10 - assignments

--- python/day_10/assignments.py
+++ python/day_10/assignments.py
@@ -1,4 +1,10 @@
-# Iterate 0 to 10 using for loop, do the same using while loop.
+# Iterate 0 to 10 using for loop, do the same using for loop.
 
 for num in range(0, 11):
     print(num)
+    
+# Iterate 0 to 10 using for loop, do the same using while loop.
+num = 0
+while num <= 10:
+    print(num)
+    num+=1
