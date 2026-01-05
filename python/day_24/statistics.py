f21436ea5bc595ce0ebf556f38d10892e2d3ec6a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-05 05:45:21 +0530

    day_24: statistics in python

--- python/day_24/statistics.py
+++ python/day_24/statistics.py
@@ -2,3 +2,18 @@ import numpy as np
 
 print('numpy:', np.__version__)
 #print(dir(np))
+
+# Creating numpy arrays
+python_list = [4,5,6,7,8,9]
+print('Type:', type (python_list))
+print(python_list)
+
+two_dimensional_list = [[0,1,2], [3,4,5], [6,7,8]]
+print(two_dimensional_list)  # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+numpy_array_from_list = np.array(python_list)
+print(type (numpy_array_from_list)) # <class 'numpy.ndarray'>
+print(numpy_array_from_list)
+
+
+
