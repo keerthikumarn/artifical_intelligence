2fab8cfc35a8fb668eb815effe9bd0c366358147
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-06 13:15:35 +0530

    day_24: statistics in python

--- python/day_24/statistics.py
+++ python/day_24/statistics.py
@@ -24,3 +24,15 @@ print(numy_array_from_list2) # array([1., 2., 3., 4., 5.])
 # Creating boolean numpy arrays
 numpy_bool_array = np.array([0, 1, -1, 0, 0], dtype=bool)
 print(numpy_bool_array) # array([False,  True,  True, False, False])
+
+# Creating multidimensional array using numpy
+two_dimensional_list = [[0,1,2], [3,4,5], [6,7,8]]
+numpy_two_dimensional_list = np.array(two_dimensional_list)
+print(type (numpy_two_dimensional_list))
+print(numpy_two_dimensional_list)
+
+# Converting numpy array to list
+np_to_list = numpy_array_from_list.tolist()
+print(type (np_to_list))
+print('one dimensional array:', np_to_list)
+print('two dimensional array: ', numpy_two_dimensional_list.tolist())
