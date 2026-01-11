f5886bac02fc8dd459fe156dd272dac959e559cd
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-11 21:33:12 +0530

    day_24: converting to various types

--- python/day_24/statistics.py
+++ python/day_24/statistics.py
@@ -183,6 +183,13 @@ print(numpy_zeroes)
 numpy_ones = np.ones((3,3),dtype=int,order='C')
 print(numpy_ones)
 
+# Reshape
+# numpy.reshape(), numpy.flatten()
+first_shape  = np.array([(1,2,3), (4,5,6)])
+print(first_shape)
+reshaped = first_shape.reshape(3,2)
+print(reshaped)
+
 
 
 
