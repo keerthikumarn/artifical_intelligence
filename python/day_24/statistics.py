ad9c8606e259345d4856da74439b83d6b6216190
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-12 21:33:12 +0530

    day_24: converting to various types

--- python/day_24/statistics.py
+++ python/day_24/statistics.py
@@ -199,6 +199,7 @@ np_list_one = np.array([1,2,3])
 np_list_two = np.array([4,5,6])
 print(np_list_one + np_list_two)
 print('Horizontal Append :', np.hstack((np_list_one, np_list_two)))
+print('vertical Append :', np.vstack((np_list_one, np_list_two)))
 
 
 
