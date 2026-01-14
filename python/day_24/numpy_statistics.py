83b3d360da74b2909d9d64e2246d96c62f60686a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-14 21:33:12 +0530

    day_24: random numbers

--- python/day_24/numpy_statistics.py
+++ python/day_24/numpy_statistics.py
@@ -14,3 +14,6 @@ plt.hist(normal_array, color="grey", bins=50)
 # Matrix in numpy
 four_by_four_matrix = np.matrix(np.ones((4,4), dtype=float))
 print(four_by_four_matrix)
+
+np.asarray(four_by_four_matrix)[2] = 2
+print(four_by_four_matrix)
