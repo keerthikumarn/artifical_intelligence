af28c01ebe6876860ef42e25c7f121bc72b7f2fe
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-14 21:33:12 +0530

    day_24: random numbers

--- python/day_24/numpy_statistics.py
+++ python/day_24/numpy_statistics.py
@@ -1,6 +1,16 @@
-# Numpy and Statistics
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+
+normal_array = np.random.normal(
+    loc=0,      # mean
+    scale=1,    # standard deviation
+    size=10000  # number of samples
+)
 
 sns.set()
 plt.hist(normal_array, color="grey", bins=50)
+
+# Matrix in numpy
+four_by_four_matrix = np.matrix(np.ones((4,4), dtype=float))
+print(four_by_four_matrix)
