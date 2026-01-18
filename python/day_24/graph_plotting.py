96c0dc2625f6d38c558fbcc1b50002e1e2ac7664
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-18 11:34:21 +0530

    plotting the graph

--- python/day_24/graph_plotting.py
+++ python/day_24/graph_plotting.py
@@ -1,6 +1,7 @@
 from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 np_normal_dis = np.random.normal(5, 0.5, 1000) # mean, standard deviation, number of samples
 print(np_normal_dis)
@@ -14,4 +15,24 @@ print('sd: ', np.std(np_normal_dis))
 
 # plot the graph
 plt.hist(np_normal_dis, color="green", bins=21)
+plt.show()
+
+
+temp = np.array([1,2,3,4,5])
+pressure = temp * 2 + 5
+
+plt.plot(temp,pressure)
+plt.xlabel('Temperature in oC')
+plt.ylabel('Pressure in atm')
+plt.title('Temperature vs Pressure')
+plt.xticks(np.arange(0, 6, step=0.5))
+plt.show()
+
+mu = 28
+sigma = 15
+samples = 100000
+
+x = np.random.normal(mu, sigma, samples)
+ax = sns.distplot(x);
+ax.set(xlabel="x", ylabel='y')
 plt.show()
