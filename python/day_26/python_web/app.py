5e44ce59c6908f0967671cb533c13ce7ec58fe72
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-25 13:42:12 +0530

    day_26: python web

--- python/day_26/python_web/app.py
+++ python/day_26/python_web/app.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def home ():
     return '<h1>Welcome</h1>'
     
+@app.route('/about')
+def about():
+    return '<h1>About us</h1>'
+    
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
