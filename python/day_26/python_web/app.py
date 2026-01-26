7ec8f5fa6561f1f19ebbeacbace412ab89bc141a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-26 13:42:12 +0530

    day_26: python web

--- python/day_26/python_web/app.py
+++ python/day_26/python_web/app.py
@@ -4,23 +4,33 @@ Python is a general purpose programming language and it can be used for many pla
 Flask
 Flask is a web development framework written in Python. Flask uses Jinja2 template engine. Flask can be also used with other modern front libraries such as React.
 '''
-from flask import Flask, render_template
-import os
+# let's import the flask
+from flask import Flask, render_template, request, redirect, url_for
+import os # importing operating system module
 
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/') # this decorator create the home route
 def home ():
-    #return '<h1>Welcome</h1>'
-    return render_template('home.html')
-    
+    techs = ['HTML', 'CSS', 'Flask', 'Python', 'Java']
+    name = '30 Days Of Python Programming'
+    return render_template('home.html', techs=techs, name = name, title = 'Home')
+
 @app.route('/about')
 def about():
-    #return '<h1>About us</h1>'
-    return render_template('about.html')
-    
+    name = '30 Days Of Python Programming'
+    return render_template('about.html', name = name, title = 'About Us')
+
+@app.route('/post')
+def post():
+    name = 'Text Analyzer'
+    return render_template('post.html', name = name, title = name)
+
+
 if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
     port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
-
 	
 
