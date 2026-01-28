3159bcd9281af0f02670623f05d3f9a05c654524
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-28 11:32:55 +0530

    started with pythong wiht mongodb

--- python/day_27/app.py
+++ python/day_27/app.py
@@ -6,6 +6,11 @@ MONGODB_URI = "mongodb+srv://admin:mydb123@python-in-30-days.coxwj7q.mongodb.net
 client = MongoClient(MONGODB_URI)
 print(client.list_database_names())
 
+# Creating a database and collection
+db = client.thirty_days_of_python
+db.employees.insert_one({'name': 'Pradeep C', 'country': 'India', 'city': 'Bangalore', 'age': 36})
+print(client.list_database_names())
+
 app = Flask(__name__)
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5002))
