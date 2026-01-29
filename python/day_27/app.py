688d187bdc5bc592bd3e2b34042e3bb7bb6aa81b
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-01-29 11:32:55 +0530

    started with pythong wiht mongodb

--- python/day_27/app.py
+++ python/day_27/app.py
@@ -11,7 +11,17 @@ db = client.thirty_days_of_python
 db.employees.insert_one({'name': 'Pradeep C', 'country': 'India', 'city': 'Bangalore', 'age': 36})
 print(client.list_database_names())
 
+# Inserting many documents to collection
+students = [
+        {'name':'Keerthi Kumar N','country':'IN','city':'Bangalore','age':40},
+        {'name':'Pradeep','country':'FR','city':'Nice','age':36},
+        {'name':'Chenna Reddy','country':'UK','city':'London','age':39},
+    ]
+
+for student in students:
+   db.students.insert_one(student)
+
 app = Flask(__name__)
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5002))
-   app.run(debug=True, host='localhost', port=port)
+   app.run(debug=False, host='localhost', port=port)
